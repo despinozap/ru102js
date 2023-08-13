410082c46fc85fd16e7855acd6668937d7534526
David Espinoza <despinozap.dev@gmail.com>
2023-08-13 15:15:19 -0400

    feat: Challenge 6 solution added

--- src/daos/impl/redis/feed_dao_redis_impl.js
+++ src/daos/impl/redis/feed_dao_redis_impl.js
@@ -117,16 +117,16 @@ const unpackStreamEntries = (streamResponse) => {
 const insert = async (meterReading) => {
   // Unpack meterReading into array of alternating key
   // names and values for addition to the stream.
-  /* eslint-disable no-unused-vars */
   const fields = objectToArray(meterReading);
-  /* eslint-enable */
-
+    
   const client = redis.getClient();
   const pipeline = client.batch();
-
+    
   // START Challenge #6
+  pipeline.xadd(keyGenerator.getGlobalFeedKey(), 'MAXLEN', '~', globalMaxFeedLength, '*', ...fields);
+  pipeline.xadd(keyGenerator.getFeedKey(meterReading.siteId), 'MAXLEN', '~', siteMaxFeedLength, '*', ...fields);
   // END Challenge #6
-
+    
   await pipeline.execAsync();
 };
 
